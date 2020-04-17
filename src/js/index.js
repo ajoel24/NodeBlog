47765094a48570cf182135178b6727738af4900d
Andrew2432 <drew.devops@gmail.com>
2020-04-17 22:22:38 +0530

    Created a build script. Added index.ejs and rendered it in index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
-const pathToViews = path.join(__dirname, "public", "views");
-
 app.set("view engine", "ejs");
+
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-	res.render(pathToViews + "index.ejs");
+	res.render("index");
 });
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
