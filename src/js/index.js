b7e4b163e39d3e5f7f3ca4e9e536e8de56cdda92
Andrew2432 <drew.devops@gmail.com>
2020-04-18 08:57:12 +0530

    Added content to home page

--- src/js/index.js
+++ src/js/index.js
@@ -8,13 +8,17 @@ app.use(
 		extended: true,
 	})
 );
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 const PORT = process.env.PORT || 3000;
+const homeText =
+	"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur dolorum, saepe inventore quo dolores beatae totam sunt, ipsa numquam neque libero voluptates non illum modi. Minus laboriosam ipsum soluta quam.";
 
 app.get("/", (req, res) => {
-	res.render("index");
+	res.render("index", {
+		homeText: homeText,
+	});
 });
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
