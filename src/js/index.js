39590ada80e3f1273885d61ba12914bc954372e5
Andrew2432 <drew.devops@gmail.com>
2020-04-18 08:47:36 +0530

    Added body-parser and made public directory available

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 const PORT = process.env.PORT || 3000;
